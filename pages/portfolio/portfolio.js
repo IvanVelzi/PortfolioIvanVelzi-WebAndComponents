f34c12152dc376571e55b6a17a462531d84698e1
IvanVelzi <Ivanvelzi9@gmail.com>
2026-02-16 17:09:27 -0300

    Arreglos

--- pages/portfolio/portfolio.js
+++ pages/portfolio/portfolio.js
@@ -1,6 +1,3 @@
-const spaceId = 'f2hj6ibejbaj';
-const accessToken = 'Q9pjpmP1ad7nLBaPHKLm3KcXZJ0LgWiPum4IuDs73LY';
-
 const url = `https://cdn.contentful.com/spaces/f2hj6ibejbaj/environments/master/entries?access_token=Q9pjpmP1ad7nLBaPHKLm3KcXZJ0LgWiPum4IuDs73LY&content_type=pagePortfolio&include=3`;
 const urlWork = `https://cdn.contentful.com/spaces/f2hj6ibejbaj/environments/master/entries?access_token=Q9pjpmP1ad7nLBaPHKLm3KcXZJ0LgWiPum4IuDs73LY&content_type=workSection&include=3`;
 
@@ -127,7 +124,6 @@ async function loadComponent(id, file, scriptFile = 0) {
   }
 }
 
-loadComponent("header", "components/header.html", "components/header.js");
-
-loadComponent("footer", "components/footer.html");
+loadComponent("header", "../../components/header.html", "../../components/header.js");
+loadComponent("footer", "../../components/footer.html");
 
